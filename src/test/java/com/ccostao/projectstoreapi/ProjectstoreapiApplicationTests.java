c19e7d3ff12209f34efac0640ac06129826eb8ce
Claudio Junior <Cjrnew@gmail.com>
2021-01-07 22:33:57 -0300

    Created project

---
+++ src/test/java/com/ccostao/projectstoreapi/ProjectstoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccostao.projectstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectstoreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
