e5ef30fb521678edb76b97bab5f46ea29b7851f6
Claudio Junior <Cjrnew@gmail.com>
2021-02-21 15:00:46 -0300

    Add DELETE into CategoryController + DataIntegretyException

--- src/main/java/com/ccostao/projectstoreapi/controller/CategoryController.java
+++ src/main/java/com/ccostao/projectstoreapi/controller/CategoryController.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -23,19 +25,19 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<Category>> categoryList() {
 		List<Category> category = categoryService.findAll();
 		return ResponseEntity.ok().body(category);
 	}
 	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	@GetMapping(value="/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Integer id) {
 		Category category = categoryService.find(id);
 		return ResponseEntity.ok().body(category);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Category category) {
 		category = categoryService.insert(category);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -43,11 +45,16 @@ public class CategoryController {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@PutMapping
-	@RequestMapping(value="/{id}")
-	public ResponseEntity<Void> update(@RequestBody Category category, @PathVariable Integer id){
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody Category category, @PathVariable Integer id) {
 		category.setId(id);
 		category = categoryService.update(category);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		categoryService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/ccostao/projectstoreapi/service/CategoryService.java
+++ src/main/java/com/ccostao/projectstoreapi/service/CategoryService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ccostao.projectstoreapi.domain.Category;
 import com.ccostao.projectstoreapi.repository.CategoryRepository;
+import com.ccostao.projectstoreapi.service.exception.DataIntegrityException;
 import com.ccostao.projectstoreapi.service.exception.ObjectNotFoundException;
 
 @Service
@@ -34,6 +36,15 @@ public class CategoryService {
 	public Category update(Category category) {
 		find(category.getId());
 		return categoryRepository.save(category);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoryRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("You cannot delete a category that has products.");
+		}
 	}
 	
 }
